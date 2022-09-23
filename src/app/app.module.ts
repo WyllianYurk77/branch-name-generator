f6fed442e2690b77565e9ec24fd6f85ced9be299
Wyllian Yurk <wyllian.mendes@dcx.ufpb.br>
2022-09-23 11:03:34 -0300

    Implementando formulário inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+branchName:any;
+name: string = '';
+
+  constructor() {  }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+
+  createForm(): void {
+    this.branchName = new FormGroup({
+      number: new FormControl('', [Validators.required, Validators.maxLength(5)]),
+      keywordOne: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]),
+      keywordTwo: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]),
+      keywordThree: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]),
+      keywordFour: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')])
+    });
+  }
+
+  buildBranchName(): void {
+    let num = this.branchName.get('number').value;
+    let keyOne = this.branchName.get('keywordOne').value;
+    let keyTwo = this.branchName.get('keywordTwo').value;
+    let keyThree = this.branchName.get('keywordThree').value;
+    let keyFour = this.branchName.get('keywordFour').value;
+    let concat = `trf-SIGEDUC-${num}-${keyOne.toLowerCase()}-${keyTwo.toLowerCase()}-${keyThree.toLowerCase()}`;
+    if(keyFour != '') {
+      concat += `-${keyFour.toLowerCase()}`;
+    }
+    this.name = concat.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    console.log(this.name);
+  }
 }
