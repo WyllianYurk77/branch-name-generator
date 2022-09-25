37f85a9d95e532814c94d30ec0c1b3d75d14cac3
Wyllian Yurk <wyllian.mendes@dcx.ufpb.br>
2022-09-24 23:12:52 -0300

    Creating the validation methods

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,8 @@
-import { SharedComponentService } from './../shared-component-service';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ResultDialogComponent } from './../result-dialog/result-dialog.component';
+import { SharedComponentService } from './../shared-component-service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,10 +10,10 @@ import { ResultDialogComponent } from './../result-dialog/result-dialog.componen
 })
 export class HomeComponent implements OnInit {
 
-branchName:any;
-name: string;
+  branchName:any;
+  name: string;
 
-  constructor(public dialog: MatDialog, public sharedComponentService: SharedComponentService) {
+  constructor(public dialog: MatDialog, public sharedComponentService: SharedComponentService, private fb: FormBuilder) {
     this.name = '';
    }
 
@@ -21,38 +21,75 @@ name: string;
     this.createForm();
   }
 
-
   createForm(): void {
-    this.branchName = new FormGroup({
-      number: new FormControl('', [Validators.required, Validators.maxLength(5)]),
-      keywordOne: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]),
-      keywordTwo: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]),
-      keywordThree: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]),
-      keywordFour: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')])
+    this.branchName = this.fb.group({
+      number: ['', [Validators.required, Validators.maxLength(5)]],
+      keywordOne: ['', [Validators.required]],
+      keywordTwo: ['', [Validators.required]],
+      keywordThree: ['', [Validators.required]],
+      keywordFour: ['']
     });
   }
 
   buildBranchName(): void {
-    let num = this.branchName.get('number').value;
-    let keyOne = this.branchName.get('keywordOne').value;
-    let keyTwo = this.branchName.get('keywordTwo').value;
-    let keyThree = this.branchName.get('keywordThree').value;
-    let keyFour = this.branchName.get('keywordFour').value;
-    let concat = `trf-SIGEDUC-${num}-${keyOne.toLowerCase()}-${keyTwo.toLowerCase()}-${keyThree.toLowerCase()}`;
-    if(keyFour != '') {
-      concat += `-${keyFour.toLowerCase()}`;
+    if(!this.validateFields()) {
+      let concat = `trf-SIGEDUC-${this.number}-${this.keyOne.toLowerCase()}-${this.keyTwo.toLowerCase()}-${this.keyThree.toLowerCase()}`;
+      if(this.keyFour != '') {
+        concat += `-${this.keyFour.toLowerCase()}`;
+      }
+      this.name = concat.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      this.sharedComponentService.setData(this.name);
+      this.openDialog();
+    } else {
+      this.branchName.markAllAsTouched();
     }
-    this.name = concat.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    this.sharedComponentService.setData(this.name);
-    this.openDialog();
   }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(ResultDialogComponent, {
       data: this.name
     });
-    dialogRef.afterClosed().subscribe((result: any) => {
-      console.log(`Dialog result: ${result}`);
+    dialogRef.afterClosed().subscribe(() => {
+      this.branchName.reset();
+      this.createForm();
     });
   }
+
+  validateFields(): boolean {
+    if(this.number == '') {
+      return true
+    }
+    if(this.keyOne == '') {
+      return true
+    }
+    if(this.keyTwo == '') {
+      return true
+    }
+    if(this.keyThree == '') {
+      return true
+    }
+
+    return false;
+  }
+
+  get number() { return this.branchName.get('number').value; }
+
+  get keyOne() { return this.branchName.get('keywordOne').value; }
+
+  get keyTwo() { return this.branchName.get('keywordTwo').value; }
+
+  get keyThree() { return this.branchName.get('keywordThree').value; }
+
+  get keyFour() { return this.branchName.get('keywordFour').value; }
+
+  set number(number: any) { this.number = number; }
+
+  set keyOne(keyOne: any) { this.keyOne = keyOne; }
+
+  set keyTwo(keyTwo: any) { this.keyTwo = keyTwo; }
+
+  set keyThree(keyThree: any) { this.keyThree = keyThree; }
+
+  set keyFour(keyFour: any) { this.keyFour = keyFour; }
+
 }
