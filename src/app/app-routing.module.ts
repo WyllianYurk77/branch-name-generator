ead2164cc7309831bd211a7ea29116706339aab1
Wyllian Yurk <wyllian.mendes@dcx.ufpb.br>
2022-09-26 12:40:18 -0300

    Changing the route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home'},
-  { path: 'home', component: HomeComponent }
+  { path: '', pathMatch: 'full', redirectTo: ''},
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
